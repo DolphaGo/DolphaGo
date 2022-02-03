7c0fc7cb0901e485fd314008c269ff72502f7152
Doha <adamdoha@naver.com>
2022-02-04 02:45:51 +0900

    ApplicationContext 적용

--- spring/toby/toby-project/src/main/java/com/example/tobyproject/toby_1/DaoFactory.java
+++ spring/toby/toby-project/src/main/java/com/example/tobyproject/toby_1/DaoFactory.java
@@ -1,11 +1,18 @@
 package com.example.tobyproject.toby_1;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // ApplicationContext 또는 빈 팩토리가 사용할 설정 정보라는 표시
 public class DaoFactory {
+
+    @Bean // Object 생성을 담당하는 IoC용 메서드라는 표시
     public UserDao userDao() {
         UserDao userDao = new UserDao(connectionMaker());
         return userDao;
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
--- spring/toby/toby-project/src/test/java/com/example/tobyproject/toby_1/UserDaoTest.java
+++ spring/toby/toby-project/src/test/java/com/example/tobyproject/toby_1/UserDaoTest.java
@@ -3,11 +3,22 @@ package com.example.tobyproject.toby_1;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class UserDaoTest {
 
+//    @Test
+//    void test() {
+//        final UserDao dao = new DaoFactory().userDao();
+//    }
+
     @Test
     void test() {
-        final UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        final UserDao dao = context.getBean("userDao", UserDao.class);
+        final UserDao dao2 = context.getBean("userDao", UserDao.class);
+        System.out.println("dao = " + dao);
+        System.out.println("dao2 = " + dao2);
     }
 }
